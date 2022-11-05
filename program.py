57b6d0d22fab8c9ac777aca62f7e4ab8090e7b49
Nuno Martins <afonsomartins1414rgmail.com>
2022-11-05 12:06:24 +0000

    Multiple Orc creation

--- program.py
+++ program.py
@@ -5,9 +5,18 @@ from playerCharacters import PlayerCharacters
 
 class Program:
     
-    warrior = PlayerCharacters("Arthur", 32, 5, 2, 5, 2, True, ["Rushdown"])
+    characters = []
+
+    warrior = PlayerCharacters("Warrior", 32, 5, 2, 5, 2, True, ["Rushdown"])
     priest = PlayerCharacters("Priest", 20, 25, 0, 2, 6, True, ["Exorcism", "Mend"])
     
-    orc = Characters("Orc", 15, 0, 2, 2, 2, False)
+    characters.append(warrior)
+    characters.append(priest)
+    
 
-    print(warrior.name)
+    for x in range(2):
+       orc = Characters("Orc "+ str(x+1), 15, 0, 2, 2, 2, False)
+       characters.append(orc)
+
+    for x in characters:
+        print(x.name)
